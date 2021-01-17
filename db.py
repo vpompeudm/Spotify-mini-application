cf739c30c743f91284f4da32cb08b35bba068023
Vinicius Pompeu de Moraes <vinicius.pompeu.dm@gmail.com>
2021-01-17 20:56:27 +0100

    insert artist working

--- db.py
+++ db.py
@@ -1,3 +1,14 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 client = MongoClient(os.getenv('MONGO_CONN'))
+
+def insert_artist(artist_name):
+    db = client['Spotify']
+    artists_collection = db['Artists']
+
+    post = {'name':artist_name}
+    artists_collection.insert(post)
