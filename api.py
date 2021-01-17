797c71252c12a283e173e3742db565d6b1041056
Vinicius Pompeu de Moraes <vinicius.pompeu.dm@gmail.com>
2021-01-17 20:29:38 +0100

    connection with db

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from markupsafe import escape
 from flask import request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print(os.getenv('MONGO_CONN'))
 
 app = Flask(__name__)
 
